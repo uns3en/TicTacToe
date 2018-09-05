57e67dd3d9bcbc9dbc2443ee891da791ef7c525a
uns3en <42949384+uns3en@users.noreply.github.com>
2018-09-05 23:12:58 +0100

    minor progress

--- ttt-gui.py
+++ ttt-gui.py
@@ -1,20 +1,41 @@
-import os, platform, pygame
+#################################################
+#...............................................#
+#.......... Second project by uns3en ...........#
+#.......... https://github.com/uns3en ..........#
+#........ A simple game of Tic-Tac-Toe .........#
+#............... now with GUI ..................#
+#............ Licence: GNU GPLv2 ...............#
+# ..............................................#
+#################################################
+
+#imports
+import os, pygame
 pygame.init()
 
+#declaring vars
 windowHeight = 450
 windowWidth = 450
+#fieldVal = ['0','0','0','0','0','0','0','0','0']
 
+#defining main window size
 gameWindow = pygame.display.set_mode([windowWidth, windowHeight])
-pygame.display.set_caption('TicTacToe', 'Xs vs 0s. Fight!')
-#gameWindow.fill((0, 0, 0))
+pygame.display.set_caption('Tic-Tac-Toe', 'Xs vs 0s. Fight!')
 run = True
-bgImage = pygame.image.load("img/field.png").convert()
+bgImage = pygame.image.load("img/field.png")
+xMark = pygame.image.load("img/x-mark.png")
+oMark = pygame.image.load("img/o-mark.png")
 
+#main loop
 while run:
+	#display game window with an image background
 	gameWindow.fill(0)
 	gameWindow.blit(bgImage, (0, 0))
+	gameWindow.blit(xMark, (0, 0))
+	gameWindow.blit(oMark, (150, 150))
 	pygame.display.flip()
+
+	#quit in even of closing game (break infinite loop without having to kill the python process)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
-			exit(0) 
+			exit(0)
