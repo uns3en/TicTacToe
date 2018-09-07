800a81c9165feb25f23ae398854f6095db883de5
teh.f4ll3n <42949384+uns3en@users.noreply.github.com>
2018-09-07 16:40:59 +0100

    updates

--- ttt-gui.py
+++ ttt-gui.py
@@ -10,25 +10,31 @@
 
 #imports
 import os, pygame
+import time
 pygame.init()
 
 #declaring vars
 windowHeight = 550
 windowWidth = 450
+player = 1
+turn = 0
+winner = 0
+run = True
+result = True
+WHITE = (255, 255, 255)
 fieldVal = ([[10, 20, 30], 
 			 [40, 50, 60], 
 			 [70, 80, 90]])
-player = 1
-turn = 0
 
-#defining main window size
+#defining main window size and caption
 gameWindow = pygame.display.set_mode([windowWidth, windowHeight])
 pygame.display.set_caption('Tic-Tac-Toe', 'Xs vs Os. Fight!')
-run = True
+
+#define image vars
 bgImage = pygame.image.load("img/field.png")
 xMark = pygame.image.load("img/x-mark.png")
 oMark = pygame.image.load("img/o-mark.png")
-WHITE = (255, 255, 255)
+
 
 #define check win conditions method
 def checkWinCond():
@@ -51,12 +57,15 @@ def checkWinCond():
 	else:
 		return True
 
+
+gameWindow.fill(0)
+gameWindow.blit(bgImage, (0, 0))
+
 #defining main drawing method
 def draw_field():
 	#display game window with an image background
-	gameWindow.fill(0)
-	gameWindow.blit(bgImage, (0, 0))
 	pygame.draw.line(gameWindow, WHITE, [0, 451], [450, 451], 2)
+	pygame.font.SysFont('Helvetica', 10)
 	for i in range(0,3):
 		for j in range (0,3):
 			if fieldVal[i][j] == 'x':
@@ -65,26 +74,47 @@ def draw_field():
 				gameWindow.blit(oMark, (i*150, j*150))
 			j = j + 1
 		i = i + 1
-	pygame.display.flip()
+	pygame.display.update()
+
 
 #main loop
 while run and turn < 9:
-	draw_field()
-	run = checkWinCond()
+	try:
+		draw_field()
+		for event in pygame.event.get():
+			#quit in even of closing game (break infinite loop without having to kill the python process)
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				exit(0)
+			# get mouse click coords
+			if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+				mousePosX, mousePosY = pygame.mouse.get_pos()
+				mousePos = mousePosX, mousePosY
+				if player == 1 and fieldVal[mousePosX//150][mousePosY//150] not in ['x', 'o']:
+					fieldVal[mousePosX//150][mousePosY//150] = 'x'
+					turn = turn + 1
+					run = checkWinCond()
+					if run == False:
+						winner = 1
+					player = 2
+				elif player == 2 and fieldVal[mousePosX//150][mousePosY//150] not in ['x', 'o']:
+					fieldVal[mousePosX//150][mousePosY//150] = 'o'
+					turn = turn + 1
+					run = checkWinCond()
+					if run == False:
+						winner = 2
+					player = 1
+	except KeyboardInterrupt:
+		break
+
+if run == False:
+	print('Player ' + str(winner) + ' wins!')
+elif turn == 9:
+	print("It's a tie!")
+draw_field()
+while result:
 	for event in pygame.event.get():
 		#quit in even of closing game (break infinite loop without having to kill the python process)
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit(0)
-		# get mouse click coords
-		if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-			mousePosX, mousePosY = pygame.mouse.get_pos()
-			mousePos = mousePosX, mousePosY
-			if player == 1:
-				fieldVal[mousePosX//150][mousePosY//150] = 'x'
-				turn = turn + 1
-				player = 2
-			else:
-				fieldVal[mousePosX//150][mousePosY//150] = 'o'
-				turn = turn + 1
-				player = 1
